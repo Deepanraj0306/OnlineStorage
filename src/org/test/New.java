be0ffc4079966178394193fe8bc00cfb38c60b27
Deepanraj0306 <deepanraj030696@gmail.com>
2022-03-04 19:25:46 +0530

    Folder Structure

---
+++ src/org/test/New.java
@@ -0,0 +1,10 @@
+package org.test;
+
+public class New {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Github processing");
+		System.out.println("Github practice");
+	}
+}
